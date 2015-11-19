d96bb0d4032000760bfe078c92bba5c15128bb28
lakshmanp <lakshmanp@b155e813-a116-496c-8e93-804e2fb32d63>
2015-11-19 13:56:30 +0000

    Removed  logs

--- CCAMobileNewBuild/www/js/index.js
+++ CCAMobileNewBuild/www/js/index.js
@@ -1,73 +1,3 @@
-//var globalspace = new Object();
-//var deviceAgent = '';
-//var bodyScroll,modalScroll;
-//var notificationTitle = "CCA";
-//var timeInterval = 30 *1000; // Splash screen sleep out time = 30 sec
-//var childBrowser;
-//var distance = 25;
-//var splashIntervalId;
-//var index = 0; // To controll multiple spinners
-//
-//var app = { 
-//    initialize: function () {                  
-//    	findDevice(screen.width, screen.height);
-//    	if(deviceAgent == "PC"){
-//            onDeviceReady(); //this is web browser
-//    	} else if (navigator.userAgent.match(/(iPhone|iPod|iPad|Android|BlackBerry)/)) {   //            
-//            document.addEventListener("deviceready", onDeviceReady, false);           
-//        } else{
-//            onDeviceReady(); //this is web browser
-//        } 
-//    }
-//};
-//
-//function onDeviceReady() {
-//    if (deviceAgent != "PC") {
-//    	
-//    	//to avoid double tap in droid 4.3 version
-//    	var ver=parseFloat(device.version);
-//		if(ver>=4.1 && ver < 4.4){ 
-//		 var last_click_time = new Date().getTime();
-//			document.addEventListener('click', function (e) {
-//			try{
-//			click_time = e['timeStamp'];
-//			if (click_time && (click_time - last_click_time) < 1000) {
-//			e.stopImmediatePropagation();
-//			e.preventDefault();
-//			return false;
-//			}
-//			last_click_time = click_time;
-//			}catch(err){alert(err)}
-//			}, true); 
-//		}
-//    	
-//    	
-//        if (navigator.connection.type == Connection.UNKNOWN || navigator.connection.type == Connection.NONE) {
-//            showAlert("Please check your network connection..!");
-//            return;
-//        }
-//        
-//        document.addEventListener("resume", onResume, false);
-//        
-//        if(deviceAgent == "androidPhone" || deviceAgent == "androidTablet"){
-//            document.addEventListener("backbutton", disableBackButon, false);
-//        }
-//       
-//        childBrowser = window.plugins.childBrowser; //Child Browser Initilization
-//       
-//    }
-//    
-//    window.onorientationchange = doOnOrientationChange; 
-//    
-//    initializeBodyScroll();
-//    
-//    showIndexPage();
-//}
-//
-//function onResume(){
-//    //setTimeout(doAutoLogin, 0);
-//}
-
 /*
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
@@ -107,8 +37,6 @@ var app = {
     // Bind any events that are required on startup. Common events are:
     // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function() {
-        //document.addEventListener('deviceready', this.onDeviceReady, false);        
-        
         findDevice(screen.width, screen.height);
     	if(deviceAgent == "PC"){
             onDeviceReady(); //this is web browser
@@ -123,8 +51,7 @@ var app = {
     //
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        //app.receivedEvent('deviceready');                
+    onDeviceReady: function() {        
         if (deviceAgent != "PC") {    	
     	//to avoid double tap in droid 4.3 version        
     	var ver=parseFloat(device.version);
@@ -153,7 +80,6 @@ var app = {
         if(deviceAgent == "androidPhone" || deviceAgent == "androidTablet"){        
             document.addEventListener("backbutton", disableBackButon, false);
         }        
-        //childBrowser = window.plugins.childBrowser; //Child Browser Initilization
         inAppBrowser = cordova.InAppBrowser.open; //In App Browser Initilization       
        
     }
@@ -181,5 +107,3 @@ var app = {
 function onResume(){   
     //setTimeout(doAutoLogin, 0);
 }
-
-//app.initialize();
